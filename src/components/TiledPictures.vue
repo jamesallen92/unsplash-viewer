ec64449e4b54cb384f7d9e9eab10271d21004cf3
James <james2allen@gmail.com>
2018-07-02 14:44:15 -0400

    Photos are now being properly pulled from API and displayed

--- src/components/TiledPictures.vue
+++ src/components/TiledPictures.vue
@@ -1,14 +1,33 @@
 <template lang="pug">
+  div
+    img(v-for='photo in photoList', fluid, :src='photo.urls.thumb')
 </template>
 
 <script>
-export default {
-  name: 'TiledPictures',
-  data () {
-    return {
+  import Unsplash, {toJson} from 'unsplash-js'
+  export default {
+    name: 'TiledPictures',
+    beforeRouteEnter (to, from, next) {
+      next(vm => {
+        let unsplash = new Unsplash({
+          applicationId: 'b54dd32e7bae520e8a4b5babde12de771627220dd99fff243708b2b8bd878bf6',
+          secret: '0645298443e3ef8b0b3780b7248697bef027ff1407b57b97499464ae56f72497',
+          callbackUrl: '{CALLBACK_URL}'
+        })
+
+        unsplash.photos.listPhotos(2, 30, 'latest')
+        .then(toJson)
+        .then(json => {
+          vm.photoList = json
+        })
+      })
+    },
+    data: function () {
+      return {
+        photoList: []
+      }
     }
   }
-}
 </script>
 <style scoped>
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import TiledPictures from '@/components/TiledPictures'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'TiledPictures',
+      component: TiledPictures
     }
   ]
 })
